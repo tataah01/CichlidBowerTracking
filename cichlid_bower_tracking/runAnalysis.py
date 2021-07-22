6c9c3836ce3f071cc67918b4d7f0785cec2e57e6
McGrath <pmcgrath7@ipsec-10-2-96-8.vpn.gatech.edu>
2021-07-22 10:06:04 -0400

    Test

--- cichlid_bower_tracking/runAnalysis.py
+++ cichlid_bower_tracking/runAnalysis.py
@@ -24,6 +24,7 @@ else:
 	summary_file = fm_obj.localAnalysisStatesDir + args.SummaryFile
 	fm_obj.downloadData(summary_file)
 	dt = pd.read_csv(summary_file, index_col = False, dtype = {'StartingFiles':str, 'Prep':str, 'Depth':str, 'Cluster':str, 'ClusterClassification':str,'LabeledVideos':str,'LabeledFrames': str})
+	pdb.set_trace()
 	projectIDs = list(dt[dt[args.AnalysisType] == 'False'].projectID) # Only run analysis on projects that need it
 
 if args.Workers is None:
