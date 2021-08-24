1a9853bb4b4a8b6c57a51c03fb63ebab3777cbcf
Patrick McGrath <ptmcgrat@gmail.com>
2021-08-24 10:07:22 -0400

    Test

--- cichlid_bower_tracking/unit_scripts/drive_updater.py
+++ cichlid_bower_tracking/unit_scripts/drive_updater.py
@@ -82,7 +82,7 @@ class DriveUpdater:
         folder_id = "'151cke-0p-Kx-QjJbU45huK31YfiUs6po'"  #'Public Images' folder ID
         
         file_list = drive.ListFile({'q':"{} in parents and trashed=false".format(folder_id)}).GetList()
-        print(file_list)
+        #print(file_list)
         # check if file name already exists so we can replace it
         flag = False
         count = 0
