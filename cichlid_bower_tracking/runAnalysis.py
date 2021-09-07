adf526ada45ac449f8ebe83883095ac02156de58
McGrath <pmcgrath7@ipsec-10-2-96-8.vpn.gatech.edu>
2021-09-07 17:31:07 -0400

    Test

--- cichlid_bower_tracking/runAnalysis.py
+++ cichlid_bower_tracking/runAnalysis.py
@@ -24,7 +24,6 @@ args = parser.parse_args()
 if args.ProjectIDs is not None:
     projectIDs = args.ProjectIDs  # Specified at the command line
 elif args.SummaryFile is not None:
-    pdb.set_trace()
     fm_obj = FM(summaryFile = args.SummaryFile)
     summary_file = fm_obj.localSummaryFile
     fm_obj.downloadData(summary_file)
