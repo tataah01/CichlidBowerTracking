cfe1bcaae74840d3d4b0363d25a8788d6db9ca6e
McGrath <pmcgrath7@ipsec-10-2-96-8.vpn.gatech.edu>
2021-09-08 07:46:14 -0400

    Test

--- cichlid_bower_tracking/runAnalysis.py
+++ cichlid_bower_tracking/runAnalysis.py
@@ -30,6 +30,7 @@ elif args.SummaryFile is not None:
     fm_obj.downloadData(summary_file)
     dt = pd.read_csv(summary_file, index_col = False, dtype = {'StartingFiles':str, 'Prep':str, 'Depth':str, 'Cluster':str, 'ClusterClassification':str,'LabeledVideos':str,'LabeledFrames': str, 'Summary': str})
     projectIDs = list(dt[dt[args.AnalysisType] == 'FALSE'].projectID) # Only run analysis on projects that need it
+    pdb.set_trace()
 else:
     print('either a summary file name or list of project ids must be provided. Exiting')
     sys.exit()
