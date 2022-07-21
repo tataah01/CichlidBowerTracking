377150a7ed9c1ca7c48292a6d393ee4f508b2253
McGrath <pmcgrath7@ipsec-10-2-98-226.vpn.gatech.edu>
2022-07-21 19:14:46 -0400

    Test

--- cichlid_bower_tracking/unit_scripts/prep_data.py
+++ cichlid_bower_tracking/unit_scripts/prep_data.py
@@ -4,11 +4,11 @@ from cichlid_bower_tracking.helper_modules.file_manager import FileManager as FM
 
 parser = argparse.ArgumentParser(usage = 'This script will prompt user to identify tray location and registration info between depth and video data. Works on a single projectID')
 parser.add_argument('ProjectID', type = str, help = 'Manually identify the project you want to analyze')
-parser.add_argument('--SummaryFile', type = str, help = 'Manually identify the project you want to analyze')
+parser.add_argument('AnalysisID', type = str, help = 'Manually identify the project you want to analyze')
 
 args = parser.parse_args()
 
-fileManager = FM(projectID = args.ProjectID, summaryFile=args.SummaryFile)
+fileManager = FM(projectID = args.ProjectID, summaryFile=args.AnalysisID)
 
 prp_obj = PrP(fileManager = fileManager)
 prp_obj.validateInputData()
