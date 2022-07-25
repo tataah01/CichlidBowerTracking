a3568a5938f494b2c8a4d23c5bbd2fa116a87eed
McGrath <pmcgrath7@ipsec-10-2-226-141.vpn.gatech.edu>
2022-07-25 12:35:03 -0400

    Changing remote data

--- cichlid_bower_tracking/helper_modules/file_manager.py
+++ cichlid_bower_tracking/helper_modules/file_manager.py
@@ -2,7 +2,7 @@ import os, subprocess, pdb, platform, shutil
 from cichlid_bower_tracking.helper_modules.log_parser import LogParser as LP
 
 class FileManager():
-	def __init__(self, projectID = None, modelID = None, analysisID=None, rcloneRemote = 'cichlidVideo:', masterDir = 'McGrath/Apps/CichlidPiData/'):
+	def __init__(self, projectID = None, modelID = None, analysisID=None, rcloneRemote = 'CichlidPiData:', masterDir = ''):
 		# Identify directory for temporary local files
 		if platform.node() == 'raspberrypi' or 'Pi' in platform.node() or 'bt-' in platform.node() or 'sv-' in platform.node():
 			self._identifyPiDirectory()
