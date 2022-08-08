01675bcbd99e62a536dff5d5c745b14c813763c3
McGrath <pmcgrath7@ipsec-10-2-226-141.vpn.gatech.edu>
2022-08-08 15:31:09 -0400

    Changing remote data

--- cichlid_bower_tracking/unit_scripts/download_data.py
+++ cichlid_bower_tracking/unit_scripts/download_data.py
@@ -2,7 +2,7 @@ import argparse, sys
 from cichlid_bower_tracking.data_preparers.project_preparer import ProjectPreparer as PP
 
 parser = argparse.ArgumentParser()
-parser.add_argument('DataType', type = str, choices=['Prep','Depth','Cluster','ClusterClassification','Train3DResnet','TrainRCNN','ManualAnnotation','ManualLabelVideos','ManualLabelFrames', 'Summary','All'], help = 'What type of analysis to perform')
+parser.add_argument('DataType', type = str, choices=['Prep','Depth','Cluster','ClusterClassification','TrackFish','Train3DResnet','TrainRCNN','ManualAnnotation','ManualLabelVideos','ManualLabelFrames', 'Summary','All'], help = 'What type of analysis to perform')
 parser.add_argument('--ProjectID', type = str, help = 'Manually identify the projects you want to analyze. If All is specified, all non-prepped projects will be analyzed')
 parser.add_argument('--AnalysisID', type = str, help = 'The ID of the analysis state this project belongs to')
 parser.add_argument('--VideoIndex', type = int, help = 'Specify which video should be downloaded if Cluster analysis is to be performed')
