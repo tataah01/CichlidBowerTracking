385eb613c92542af65d95c649220dfef58b0eadb
Breanna Shi <74272793+breannashi@users.noreply.github.com>
2023-01-23 14:51:35 -0500

    Update drive_updater.py

--- cichlid_bower_tracking/unit_scripts/drive_updater.py
+++ cichlid_bower_tracking/unit_scripts/drive_updater.py
@@ -68,6 +68,7 @@ class DriveUpdater:
         return daily_bower
     
     def _createImage(self, stdcutoff = 0.1):
+        print(self.lp.frames[-1].std)
         lastHourFrames = [x for x in self.lp.frames if x.time > self.lastFrameTime - datetime.timedelta(hours = 1)] # frames from the last hour
         lastTwoHourFrames = [x for x in self.lp.frames if x.time > self.lastFrameTime - datetime.timedelta(hours = 2)] # frames from the last two hours
         daylightFrames = [x for x in self.lp.frames if x.time.hour >= 8 and x.time.hour <= 17] # frames during daylight
