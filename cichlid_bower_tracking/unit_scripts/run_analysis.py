4c214b2e7a310893c44354f764148b13d67d8351
McGrath <pmcgrath7@ipsec-10-2-225-238.vpn.gatech.edu>
2023-04-19 16:22:54 -0400

    Fix Fastq files

--- cichlid_bower_tracking/unit_scripts/run_analysis.py
+++ cichlid_bower_tracking/unit_scripts/run_analysis.py
@@ -81,7 +81,8 @@ elif args.AnalysisType == 'TrackFish':
 		for p1 in processes:
 			p1.communicate()
 			if p1.returncode != 0:
-				print('YOLO Error')
+				raise Exception('YOLO Error')
+
 
 	processes = []
 	for idx in range(len(videos)):
@@ -90,10 +91,10 @@ elif args.AnalysisType == 'TrackFish':
 	for p1 in processes:
 		p1.communicate()
 		if p1.returncode != 0:
-			print('SORT Error')
+			raise Exception('SORT Error')
 
 	from cichlid_bower_tracking.data_preparers.cluster_track_association_preparer_new import ClusterTrackAssociationPreparer as CTAP
-	ctap_obj = CTAP(self.fileManager)
+	ctap_obj = CTAP(fm_obj.fileManager)
 	ctap_obj.summarizeTracks()
 	ctap_obj.associateClustersWithTracks()
 	ctap_obj.createMaleFemaleAnnotationVideos()
