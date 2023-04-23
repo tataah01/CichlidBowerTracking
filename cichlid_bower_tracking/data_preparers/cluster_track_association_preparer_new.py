9e89048102dbcedb3f2d75f81ec6f136611365e6
McGrath <pmcgrath7@ipsec-10-2-226-62.vpn.gatech.edu>
2023-04-23 12:35:29 -0400

    Fix Fastq files

--- cichlid_bower_tracking/data_preparers/cluster_track_association_preparer_new.py
+++ cichlid_bower_tracking/data_preparers/cluster_track_association_preparer_new.py
@@ -73,7 +73,8 @@ class ClusterTrackAssociationPreparer():
 	def associateClustersWithTracks(self):
 		c_dt = pd.read_csv(self.fileManager.localAllLabeledClustersFile, index_col = 0)
 		c_dt['track_id'] = ''
-		t_dt = pd.read_csv(self.fileManager.localAllFishTracksFile, index_col = 0)
+		t_dt = pd.read_csv(self.fileManager.localAllFishTracksFile, index_col = 0, dtype={'base_name':'category'})
+
 		t_dt['delta'] = 100000
 
 		for i,cluster in c_dt.iterrows():
@@ -81,7 +82,7 @@ class ClusterTrackAssociationPreparer():
 				print(i)
 			if cluster.ClipCreated == 'Yes':
 
-				possible_tracks = t_dt[(t_dt.base_name == cluster.VideoID) & (t_dt.frame > (cluster.t-0.5)*29) & (t_dt.frame < (cluster.t+0.5)*29) ]
+				possible_tracks = t_dt[(t_dt.base_name == cluster.VideoID) & (t_dt.frame.values > (cluster.t-0.5)*29) & (t_dt.frame.values < (cluster.t+0.5)*29) ]
 				#if len(possible_tracks) == 0:
 				#	continue
 				#possible_tracks['delta'] = pow(possible_tracks['yc'] - cluster.X,2) + pow(possible_tracks['xc'] - cluster.Y, 2)
